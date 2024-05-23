8d413a4806e1e0a0186cecf4e84d03cc79a20a98
goott3 <goott3@DESKTOP-I2BU47B>
2024-05-23 16:58:02 +0900

    공지사항 리스트 및 상세정보, 페이징 처리 기능 구현 커밋

--- Team2Project/src/main/java/com/Team2Project/WorkWave/controller/NoticeController.java
+++ Team2Project/src/main/java/com/Team2Project/WorkWave/controller/NoticeController.java
@@ -1,12 +1,19 @@
 package com.Team2Project.WorkWave.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.Team2Project.WorkWave.model.NoticeDTO;
 import com.Team2Project.WorkWave.model.NoticeMapper;
+import com.Team2Project.WorkWave.model.Page;
 
-
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class NoticeController {
@@ -14,5 +21,46 @@ public class NoticeController {
 	@Autowired
 	private NoticeMapper mapper;
 	
+	// 한 페이지당 보여질 게시물의 수
+	private final int rowsize = 10;
+	
+	// DB 상의 전체 게시물의 수
+	private int totalRecord = 0;
+	
+	@GetMapping("notice.go")
+	public String noticeList(HttpServletRequest request, Model model) {
+		
+		int page;	// 현재 페이지 변수
+		
+		// 페이징 처리 작업
+		if(request.getParameter("page") != null) {
+			page = Integer.parseInt(request.getParameter("page"));
+		}else {
+			page = 1;
+		}
+		
+		totalRecord = this.mapper.countNotices();
+		
+		Page pdto = new Page(page, rowsize, totalRecord);
+		
+		// 현재 페이지에 해당하는 공지사항을 가져오는 메서드 호출.
+		List<NoticeDTO> notice_list = this.mapper.noticeList(pdto);
+		
+		model.addAttribute("nList", notice_list)
+			 .addAttribute("paging", pdto);
+		
+		return "notice/list";
+	}
+	
+	@GetMapping("notice_cont")
+	public String noticeCont(@RequestParam("no") int no, Model model) {
+		
+		// 공지사항 상세정보 호출
+		NoticeDTO notice_cont = this.mapper.noticeCont(no);
+		
+		model.addAttribute("nCont", notice_cont);
+		
+		return "notice/cont";
+	}
 	
 }
--- Team2Project/src/main/java/com/Team2Project/WorkWave/model/NoticeMapper.java
+++ Team2Project/src/main/java/com/Team2Project/WorkWave/model/NoticeMapper.java
@@ -1,8 +1,17 @@
 package com.Team2Project.WorkWave.model;
 
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface NoticeMapper {
+	
+	List<NoticeDTO> noticeList(Page pdto);
+	
+	int countNotices();
+	
+	NoticeDTO noticeCont(int no);
+	
 }
