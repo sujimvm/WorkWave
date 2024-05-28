6f61115eef6b275ea8767d36a46d1d55da4dba9a
suji <sujimvm@naver.com>
2024-05-28 18:53:18 +0900

    header / footer 수정 공고리스트 추가 중 js / css 폴더 추가

--- Team2Project/src/main/java/com/Team2Project/WorkWave/model/ComBoardDTO.java
+++ Team2Project/src/main/java/com/Team2Project/WorkWave/model/ComBoardDTO.java
@@ -25,6 +25,23 @@ public class ComBoardDTO {
 	private String com_board_image3;
 	private String com_board_date;
 
-
-    // 생성자, Getter 및 Setter 생략 (필요시 추가)
+	//기업정보
+	private String company_id;
+	private String company_name;
+	private String company_pwd;
+	private String company_addr;
+	private String company_phone;
+	private String company_homepage;
+	private String company_regdate;
+	private String company_join_date;
+	private String company_ceo;
+	private String company_confirm;
+	private String company_emp_count;
+	private String company_sal;
+	private String company_number;
+	private String com_board_logo;
+	private String company_mgr_name;
+	private String company_mgr_phone;
+	private String company_mgr_email;
+	
 }
--- Team2Project/src/main/java/com/Team2Project/WorkWave/model/ComBoardMapper.java
+++ Team2Project/src/main/java/com/Team2Project/WorkWave/model/ComBoardMapper.java
@@ -1,8 +1,17 @@
 package com.Team2Project.WorkWave.model;
 
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
 
 @Mapper
 public interface ComBoardMapper {
+	List<ComBoardDTO> getJobList();
+	List<CodeDTO> getJobCodeGroupList();
+	List<CodeDTO> getJobCodeSubList();
+	List<CodeDTO> getJobCodeStepList();
+	List<CodeDTO> getLocationCodeGroupList();
+	List<CodeDTO> getLocationCodeSubList();
 }
