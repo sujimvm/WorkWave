7106bbdfa4501cbdcfea3890221496e5e90fbf51
sujimvm <sujimvm@naver.com>
2024-06-05 23:41:15 +0900

    Update UserSecurityConfig.java

--- Team2Project/src/main/java/com/Team2Project/WorkWave/config/UserSecurityConfig.java
+++ Team2Project/src/main/java/com/Team2Project/WorkWave/config/UserSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.Team2Project.WorkWave.config;
+package com.Team2Project.WorkWave.config; 
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
