33ed87d9accc1a92efc07eb9faa72239c841897b
suhyun <qotngus31179@naver.com>
2024-06-08 18:54:05 +0900

    수정

--- Team2Project/src/main/java/com/Team2Project/WorkWave/controller/CUController.java
+++ Team2Project/src/main/java/com/Team2Project/WorkWave/controller/CUController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.Team2Project.WorkWave.model.CareerDTO;
@@ -22,7 +23,8 @@ import com.Team2Project.WorkWave.model.ProfileMapper;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Controller("/CU")
+@Controller
+@RequestMapping("/CU")
 public class CUController {
 
 	@Autowired
