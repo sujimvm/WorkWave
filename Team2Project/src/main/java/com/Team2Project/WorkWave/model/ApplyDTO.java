b02c40b1366a5e7a82a11a99bd5ed42e61dd11f6
goott3 <goott3@DESKTOP-I2BU47B>
2024-06-16 00:18:01 +0900

    입사지원 리스트 쿼리문 수정중

--- Team2Project/src/main/java/com/Team2Project/WorkWave/model/ApplyDTO.java
+++ Team2Project/src/main/java/com/Team2Project/WorkWave/model/ApplyDTO.java
@@ -9,7 +9,9 @@ public class ApplyDTO {
 	private int com_board_key;
 	private String apply_date;
 	private String apply_profile_check;
-
+	private String apply_check;
+    private int user_key;
+    private int company_key;
 
     // 생성자, Getter 및 Setter 생략 (필요시 추가)
 }
