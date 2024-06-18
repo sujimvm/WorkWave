af39d99fdc6004932ebc06b60030905453970519
goott3 <goott3@DESKTOP-I2BU47B>
2024-06-18 18:08:17 +0900

    user - position 제안 리스트 기능 구현

--- Team2Project/src/main/java/com/Team2Project/WorkWave/model/PositionDTO.java
+++ Team2Project/src/main/java/com/Team2Project/WorkWave/model/PositionDTO.java
@@ -4,14 +4,16 @@ import lombok.Data;
 
 @Data
 public class PositionDTO {
-	private String position_key;
-	private String user_key;
-	private String company_key;
-	private String com_board_key;
+	private int position_key;
+	private int user_key;
+	private int company_key;
+	private int com_board_key;
 	private String position_title;
 	private String position_cont;
 	private String position_check;
 	private String position_date;
+	// 마감일
+	private String expirationDate;
 
     // 생성자, Getter 및 Setter 생략 (필요시 추가)
 }
