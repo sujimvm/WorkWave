cbd487ff895cf01ad44bb8175f6beaedc28312a0
suji <sujimvm@naver.com>
2024-06-24 00:41:23 +0900

    상세보기 CSS

--- Team2Project/src/main/resources/static/js/comBoard/content.js
+++ Team2Project/src/main/resources/static/js/comBoard/content.js
@@ -88,63 +88,91 @@ window.open("/CU/profile/content?no="+num);
 // 공고리스트 조회 및 출력
 function getRecommendList(page) {
 	
-	$.ajax({
-		url: '/ajax/recommendList',
-		type: 'post',
-		dataType: 'json',
-		data:{"page":page,"code":$('#code').val(),"comBoardKey":$('#com_board_key').val()},
-		success: function(map) {
-			$('#recommendList').empty(); // Clear any existing rows
-			$('#pagination').empty();
-			var list = map.list;
-			var successList = map.successList;
-			var paging = map.paging;
-			var paging_li = "";
-			if(list.length > 0){
-				list.forEach(function(list) {
-					var row = "<tr>"+
-					"<td rowspan='3'><img src='/image/profile/"+ list.profile_image +"'></td>"+
-					"<td>"+ list.user_name +"</td>"+
-					"<td rowspan='3'><input type='button' id='positionBt_"+ list.user_key +"' value='포지션제안' onclick='positionInputModal("+ list.user_key +",\""+ list.user_name +"\")'></td>"+
-					"</tr>"+
-					"<tr><td><a href='javascript:profileView(\""+ list.profile_key +"\")'>"+ list.profile_name +"</a></td></tr>"+
-					"<tr><td>"+ list.profile_group1 +list.profile_sub1 +list.profile_step1 +list.profile_group2 +list.profile_sub2 +list.profile_step2 +"</td></tr>";
+	if($('#sessionID').val()==1){
+		$.ajax({
+			url: '/ajax/recommendList',
+			type: 'post',
+			dataType: 'json',
+			data:{"page":page,"code":$('#code').val(),"comBoardKey":$('#com_board_key').val()},
+			success: function(map) {
+				$('#recommendList').empty(); // Clear any existing rows
+				$('#pagination').empty();
+				var list = map.list;
+				var successList = map.successList;
+				var paging = map.paging;
+				var paging_li = "";
+				if(list.length > 0){
+					list.forEach(function(list) {
+						var gender = (list.user_gender == 'M')? '남':'여';
+						const today = new Date();
+						const birthday = new Date(list.user_birth);
+						let age = today.getFullYear() - birthday.getFullYear();
+						
+						var row = "<tr>"+
+						"<td width='13%'><img src='/image/profile/"+ list.profile_image +"'></td>"+
+						"<td>"+
+						"<a href='javascript:profileView(\""+ list.profile_key +"\")'>"+ list.profile_name +"</a>"+
+						"<p>"+ list.user_name +"("+age+") "+ gender + "</p>"+
+						"<p>"+ list.profile_group1 +list.profile_sub1 +list.profile_step1 +list.profile_group2 +list.profile_sub2 +list.profile_step2 +"</p>"+
+						"</td>"+
+						"<td width='12%'><input type='button' class='btCss4' id='positionBt_"+ list.user_key +"' value='포지션제안' onclick='positionInputModal("+ list.user_key +",\""+ list.user_name +"\")'></td>"+
+						"</tr>";
+						
+						$('#recommendList').append(row);	
+					}); 
 					
-					$('#recommendList').append(row);	
-				}); 
-				
-				if(successList != undefined){
-					successList.forEach(function(successList) {
-						$('#positionBt_'+successList).attr("value", "요청완료");
-						$('#positionBt_'+successList).attr("disabled", true);
-					});
+					if(successList != undefined){
+						successList.forEach(function(successList) {
+							$('#positionBt_'+successList).attr("value", "요청완료");
+							$('#positionBt_'+successList).attr("disabled", true);
+						});
+					}
+				}else{
+					$('#recommendList').append("<tr><th width='100%'>추천인재가 존재하지 않습니다.<th></tr>");
 				}
-			}else{
-				$('#recommendList').append("<tr><th width='100%'>추천인재가 존재하지 않습니다.<th></tr>");
-			}
+				
+				if(paging != undefined && paging.page > paging.block){
+					paging_li += "<li><a href='javascript:getRecommendList(1)'>맨처음</a></li>";
+				}
+				if(paging != undefined && paging.page > paging.block){ 
+					paging_li += "<li><a href='javascript:getRecommendList("+(paging.startBlock-1)+")'> ← </a></li>";
+				}
+				for(var i = paging.startBlock; i<= paging.endBlock; i++){ 
+					paging_li += "<li><a href='javascript:getRecommendList("+i+")'>"+i+"</a></li>";
+				}
+				if(paging != undefined && paging.endBlock < paging.allPage){ 
+					paging_li += "<li><a href='javascript:getRecommendList("+(paging.endBlock+1)+")'> → </a></li>";
+				}
+				if(paging != undefined && paging.endBlock < paging.allPage){ 
+					paging_li += "<li><a href='javascript:getRecommendList("+paging.allPage+")'>맨뒤</a></li>";
+				}
 			
-			if(paging != undefined && paging.page > paging.block){
-				paging_li += "<li><a href='javascript:getRecommendList(1)'>맨처음</a></li>";
+				$('#pagination').append(paging_li);
+			
+			},
+			error: function(xhr, status, error) {
+				console.error(xhr);
 			}
-			if(paging != undefined && paging.page > paging.block){ 
-				paging_li += "<li><a href='javascript:getRecommendList("+(paging.startBlock-1)+")'> ← </a></li>";
-			}
-			for(var i = paging.startBlock; i<= paging.endBlock; i++){ 
-				paging_li += "<li><a href='javascript:getRecommendList("+i+")'>"+i+"</a></li>";
-			}
-			if(paging != undefined && paging.endBlock < paging.allPage){ 
-				paging_li += "<li><a href='javascript:getRecommendList("+(paging.endBlock+1)+")'> → </a></li>";
-			}
-			if(paging != undefined && paging.endBlock < paging.allPage){ 
-				paging_li += "<li><a href='javascript:getRecommendList("+paging.allPage+")'>맨뒤</a></li>";
-			}
-		
-			$('#pagination').append(paging_li);
-		
-		},
-		error: function(xhr, status, error) {
-			console.error(xhr);
-		}
-	});
+		});
+	}
 }
 
+function updateTimer() {
+    const future = Date.parse($("#com_board_end_date").val());
+    const now = new Date();
+    const diff = future - now;
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const mins = Math.floor(diff / (1000 * 60));
+    const secs = Math.floor(diff / 1000);
+
+    const d = days;
+    const h = String(hours - days * 24).padStart(2, '0');
+    const m = String(mins - hours * 60).padStart(2, '0');
+    const s = String(secs - mins * 60).padStart(2, '0');
+
+    document.getElementById("timer").innerHTML = '<div>' + d + '일 ' + h + ':' + m + ':' + s + '</div>';
+}
+
+setInterval(updateTimer, 1000);
